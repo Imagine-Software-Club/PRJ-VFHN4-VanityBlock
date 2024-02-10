ced081341288af4d59f2486683f859c905c14d99
naligant <naligant@msu.edu>
2024-02-09 19:26:15 -0500

    navbar upgrade

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,22 +1,33 @@
 import Link from "next/link";
+import "../styles/navbar.css";
 const Navbar = () => 
 {
 return(
-    <nav className = "bg-white">
-        <div className = "max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className = "flex items-center justify-between h-16">
-                <div className = "flex items-center space-x-4"> 
-                    <div className = "flex-shrink-0">
-                        <a href="/" className="text-black">
-                            <img src="/images/logo.png" alt="Logo" width={120} height={120}/>
-                        </a>
-                    </div>
-                    <a href="/listings">
-                        <img src="/images/list_your_plates.png" alt="List your plates" width={120} height={120}/>
+    <nav>
+        <div className="nav">
+            <a href="/">
+                <img src="/images/logo.png" alt="Logo" width={160} height={160}/>
+            </a>
+            <a href="/listings">
+                <img src="/images/list_your_plates.png" alt="List your plates" width={160} height={160}/>
+            </a>
+            <a href="/about">
+                <img src="/images/about.png" alt='About' width={84} height={84}/>
+            </a>
+            <div className="rightImages">
+                <div className="links">
+                    <a href="/comments">
+                        <img src="/images/comments.png" alt="Comments" width={50} height={50}/>
                     </a>
-                    <a href="/about">
-                        <img src="/images/about.png" alt='About' width={65} height={65}/>
+                    <a href="/notifications">
+                        <img src="/images/notifications.png" alt='Notifications' width={50} height={50}/>
                     </a>
+                    <a href="/signup">
+                        <img src="/images/signup.png" alt='Signup' width={100} height={100}/>
+                    </a>
+                </div>
+                <div className="menu-icon">
+                    <img src="/images/menu.png" alt="menu" width={50} height={50}/>
                 </div>
             </div>
         </div>
