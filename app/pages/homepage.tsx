9094748d08fa038140a9aa51c09add7b77361034
farkya1 <farkasa1@msu.edu>
2024-02-10 23:16:42 -0500

    made the home page bar better

--- app/pages/homepage.tsx
+++ app/pages/homepage.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ListingCard from '@/src/components/ListingCard'
 import Grid from '@mui/material/Unstable_Grid2/Grid2'
 import MainMenuHeader from '@/src/components/MainMenuHeader'
+import { Box } from '@mui/material'
 
   
 export default function Homepage() { 
+
+  
   return ( 
 
-  <div>
+  <Box
+  >
     <MainMenuHeader/>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    
-    <Grid container spacing={{ xs: 4, md: 4, lg: 4 }} >
-      {Array.from(Array(12)).map((_, index) => (
-        <Grid  key={index} xs={4}>
-          <ListingCard name={index} ></ListingCard>
-        </Grid>
-      ))}
-    </Grid>
-  </div>
-  </div>
+    <Grid
+        container       
+        justifyContent="center"
+        alignItems="center"
+      >
+        {Array.from(Array(12)).map((_, index) => (
+          <Grid  key={index} xs={4}>
+            <ListingCard name={index}  justifyContent="center" alignItems="center" ></ListingCard>
+          </Grid>
+        ))}
+      </Grid>
+
+  </Box>
 
 
     
--- src/components/MainMenuHeader.tsx
+++ src/components/MainMenuHeader.tsx
@@ -1,68 +1,85 @@
-import React from "react";
+"use client";
+import React,{useState} from "react";
 
 
-import { Box, FormControl,InputAdornment,NativeSelect, TextField } from "@mui/material";
+
+import { Box, FormControl,InputAdornment,Select, TextField,SelectChangeEvent } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2/Grid2'
+import SearchIcon from '@mui/icons-material/Search';
+
+
 
 export default function MainMenuHeader() {
+    const [age, setAge] = useState('Past');
+
+    const handleChange = (event: SelectChangeEvent) => {
+      setAge(event.target.value);
+    };
+
     return (
-        <div style={{display:"flex"}}>    
-
-            <Box sx={{ minWidth: 120 }}>
+        <Grid container columnGap={{ md:3, sm:5}}
+        justifySelf="center"
+        alignSelf="center"
+        alignItems="center"
+        justifyContent="center"
+        >
+            <Grid md={2} sm={2} xs={12} >
                 <FormControl fullWidth>
-                    <NativeSelect
-                        defaultValue={"Past"}
-                        inputProps={{
-                        name: 'age',
-                        id: 'uncontrolled-native',
-                        }}>
-                        <option value={"Past"}>Past Auctions</option>
+                    <Select
+                    autoWidth
+                    defaultValue={"Live"}
+                    
+                    >
                         <option value={"Live"}>Live Auctions</option>
-                    </NativeSelect>
+                        <option value={"Past"}>Past Auctions</option>
+                    </Select>
                 </FormControl>
-            </Box>
+            </Grid>
+            <Grid md={1.5} sm={.5} xs={0}>
 
-        <TextField
-            id="searchBar"
-            InputProps={{
-            startAdornment: (
-                <InputAdornment position="start" >
-                    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"/></svg>
-                </InputAdornment>
-            ),
-            }}
-            variant="standard"
-        />
+            </Grid>
+            <Grid md={3} sm={3} xs={12}>
+                <FormControl fullWidth>
+                    <TextField 
+                    
+                    id="searchBar"
+                    InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start" >
+                            <SearchIcon></SearchIcon>
+                        </InputAdornment>
+                    ),
+                    }}
+                    variant="standard"
+                    placeholder="Search"
+                    />      
+                </FormControl>
+            </Grid>
+            <Grid md={1} sm={2} xs={12}>
+                <FormControl fullWidth>
+                    <Select
+                    autoWidth
+                    defaultValue={"State"}>
+                        <option value={"State"}>State</option>
+                    </Select>
+                </FormControl>
+            </Grid>
 
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <NativeSelect
-                    defaultValue={"Past"}
-                    inputProps={{
-                    name: 'age',
-                    id: 'uncontrolled-native',
-                    }}>
-                    <option value={"State"}>State</option>
-                </NativeSelect>
-            </FormControl>
-        </Box>
+            <Grid md={1.5} sm={2} xs={12}>
+                <FormControl fullWidth>
+                    <Select
+                    autoWidth
+                    defaultValue={"Ending Soon"}>
+                        <option value={"Ending Soon"}>Ending Soon</option>
+                        <option value={"Ending Last"}>Ending Last</option>
+                        <option value={"Lowest Bid"}>Lowest Bid</option>
+                        <option value={"Highest Bid"}>Highest Bid</option>
+                        <option value={"Nearest to Me"}>Nearest to Me</option>
+                    </Select>
+                </FormControl>
+            </Grid>            
 
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <NativeSelect
-                    defaultValue={"Past"}
-                    inputProps={{
-                    name: 'age',
-                    id: 'uncontrolled-native',
-                    }}>
-                    <option value={"Ending Soon"}>Ending Soon</option>
-                    <option value={"Ending Last"}>Ending Last</option>
-                    <option value={"Lowest Bid"}>Lowest Bid</option>
-                    <option value={"Highest Bid"}>Highest Bid</option>
-                    <option value={"Nearest to Me"}>Nearest to Me</option>
-                </NativeSelect>
-            </FormControl>
-        </Box>
-      </div>
+        </Grid>
 
     );
   }
