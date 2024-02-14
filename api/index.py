342b1e9ca017aa190be5395bdf81e096e578e398
Swabhan Katkoori <swabhankatkoor@icloud.com>
2024-02-14 17:46:15 -0500

    Add Nitin's API Initialization

--- api/index.py
+++ api/index.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+# from firestorm import get_firestore_db
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 app = FastAPI()
+cred = credentials.Certificate('./api/credentials.json')
 
-@app.get("/api/python")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+#Get all listings in database
+@app.get("/listings")
 def hello_world():
-    return {"message": "Hello World"}
+    result = []
+
+    user = db.collection('Listings')
+    docs = user.stream()
+    
+    for doc in docs:
+        if doc.exists:
+            this_event = doc.to_dict()
+            result.append(this_event)
+        else:
+            print("Document does not exist!")
+
+    
+    return {"Listings": result}
