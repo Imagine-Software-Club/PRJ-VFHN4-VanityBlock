f7f723dd0aad2daa4b82ba35fa8f7e9f0864edfc
HungLuong05 <duchung210705@gmail.com>
2024-02-18 18:27:38 -0500

    Make ListingCard component fit to the screen

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import Box, { BoxProps } from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import { borders } from '@mui/system';
 import CardMedia from '@mui/material/CardMedia';
-import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
 const sxOuterCard = {
-  width: "27%",
-  height: "39%",
+  height: 300,
+  width: 430,
   border: "none",
   boxShadow: "none",
 };
@@ -28,8 +23,8 @@ function Item(props: BoxProps){
         bgcolor: '#1C1C1D',
         border: '2px solid',
         borderColor: '#1C1C1D',
-        borderRadius: 1,
-        fontSize: '0.7rem',
+        borderRadius: 1.5,
+        fontSize: '1.2rem',
         fontWeight: '500',
       }}
       {...other}
@@ -37,124 +32,68 @@ function Item(props: BoxProps){
   );
 }
 
-export default function ListingCard(props: any){
+function ListingCard(props: any){
   return(
-    <Card sx={{...sxOuterCard, }}>
-      <Box sx={{ position: 'relative' }}>
-        <CardMedia
-          component="img"
-          height="100%"
-          image="/images/PlateExample.jpg"
-        />
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            // bgcolor: 'rgba(0, 0, 0, 0.54)',
-            color: 'white',
-            padding: '10px',
-          }}
-        >
-          <Item> {props.time} </Item>
-          <Item> {props.price} </Item>
-        </Box>
-      </Box>
-
-      <CardContent>
-        <h1>
-          <b>
-          <center> {props.name} </center>
-          </b>
-        </h1>
-      </CardContent>
-    </Card>
-  );
-}
-
-{/*
-function Item(props: BoxProps){
-  // Enable adding sx and other configurations
-  const {sx, ...other} = props;
-
-  return(
-    <Box
-      sx = {{
-        p: 1,
-        m: 0.1,
-        position: 'absolute',
-        // bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : 'grey.100'),
-        // color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
-        border: '1px solid',
-        // borderColor: (theme) => theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-        borderRadius: 2,
-        ...sx,
-      }}
-      {...other}
-    />
-  );
-}
-
-export default function ListingCard(props: any) {
-  return (
-    <Box
-      sx={{ height: '300px', 
-            width: '300px',
-            border: '1px solid',
-            borderRadius: 10,
-            bordercolor: 'dark',
-            // backgroundImage: 'url(../../static/images/cards/PlateExample.jpg)',
-            // backgroundSize: "cover",
-      }}
-    >
-      <Card variant="outlined">  
-
-      <React.Fragment>
-        
-        <CardMedia
-          component="img"
-          height="194"
-          image="../../static/images/cards/PlateExample.jpg"
-          alt="VanityBlock101"
-        />
-
-        <div style = {styles.overlay}>
-          AAAA I want to destroy
-        </div>
-
-        <Item>props.date</Item>
-
-        <CardContent>
-
-          <center>
-          <h1>{props.name}</h1>
-          </center>
-
-          {/*
-          <Box 
+    <Link href = "listings">
+      <Card sx={{...sxOuterCard, }}>
+        <Box sx={{ position: 'relative' }}>
+          <CardMedia
+            component="img"
             sx = {{
+              objectFit: 'cover',
+              height: 300,
+              width: 430,
+            }}
+            image = "images/PlateExample.jpg"
+          />
+          
+          <Box
+            sx={{
               display: 'flex',
               justifyContent: 'space-between',
-              p: 1,
-              m: 0.1,
-              // bgcolor: 'background.paper',
-              // borderRadius: 10,
-              // bordercolor: 'dark',
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              width: '100%',
+              // bgcolor: 'rgba(0, 0, 0, 0.54)',
+              color: 'white',
+              padding: '10px',
             }}
           >
-            <Item>props.date</Item>
-            <Item>props.price</Item>
-          </Box>
+            <Item>
+              <Box sx = {{
+                display: 'flex',
+                alginItems: 'center',
+                flexWrap: 'wrap',
+              }}>
+                <Box
+                  component = "img"
+                  sx = {{
+                    maxHeight: 20,
+                    maxWidth: 20,
+                    m: 0.5,
+                  }}
+                  alt = "Clock Icon"
+                  src = "images/Clock.png"
+                >
 
+                </Box>
+                {props.time}
+              </Box> 
+            </Item>
+            <Item> {props.price} </Item>
+          </Box>
+          
+        </Box>
+
+        <CardContent>
+          <h1>
+            <center> {props.name} </center>
+          </h1>
         </CardContent>
-      
-      </React.Fragment>
-      
       </Card>
-    </Box>
+    </Link>
   );
 }
-*/}
+
+export default ListingCard;
